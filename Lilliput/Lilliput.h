62c41c954d1e9f7bf07fc9888e887523bba2355f
jkolb <franticapparatus@gmail.com>
2014-07-10 19:38:37 -0500

    Initial Commit

---
+++ Lilliput/Lilliput.h
@@ -0,0 +1,19 @@
+//
+//  Lilliput.h
+//  Lilliput
+//
+//  Created by Justin Kolb on 7/10/14.
+//  Copyright (c) 2014 Justin Kolb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Lilliput.
+FOUNDATION_EXPORT double LilliputVersionNumber;
+
+//! Project version string for Lilliput.
+FOUNDATION_EXPORT const unsigned char LilliputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lilliput/PublicHeader.h>
+
+
